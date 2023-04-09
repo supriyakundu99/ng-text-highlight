5798a046efc1920f6d1ede33165700b383461623
Supriya Kundu <supriya.kundu.1999@gmail.com>
2023-04-09 21:57:59 +0530

    Text highlighter implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { TextHighlighterComponent } from './text-highlighter/text-highlighter.component';
+import 'zone.js';
 
 @NgModule({
   declarations: [
