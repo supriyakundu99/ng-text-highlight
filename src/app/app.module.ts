6d776c7517d45951f7f20454e706c04bebac520f
Supriya Kundu <supriya.kundu.1999@gmail.com>
2025-04-15 22:34:09 +0530

    Added Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
 import "zone.js";
-import { TextHighlighterComponent } from "./text-highlighter/text-highlighter.component";
-import { FormsModule, NgModel } from "@angular/forms";
+import { FormsModule } from "@angular/forms";
+import { TextHighlighterModule } from "./text-highlighter/text-highlighter.module";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule, TextHighlighterComponent],
+  imports: [BrowserModule, FormsModule, TextHighlighterModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/text-highlighter/text-highlighter.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from "@angular/core";
+import { TextHighlighterComponent } from "./text-highlighter.component";
+
+@NgModule({
+  declarations: [],
+  imports: [TextHighlighterComponent],
+  exports: [TextHighlighterComponent],
+})
+export class TextHighlighterModule {}
