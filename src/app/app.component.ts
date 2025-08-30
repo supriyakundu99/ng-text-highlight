7ab2450cfca0bd62f44808a3b5bfd8fd8e39f4f1
Supriya Kundu <supriya.kundu.1999@gmail.com>
2025-08-30 10:07:34 +0530

    removed un used vars

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,9 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    standalone: false
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
+  standalone: false,
 })
-export class AppComponent {
-  title = 'main-app';
-  keywords: Array<string> = [];
-
-  sampleText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
-  sampleSearchText = 'et dolore magna';
-
-}
+export class AppComponent {}
